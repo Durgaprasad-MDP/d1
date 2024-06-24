91d78a42db9260570944c18003b1fb3fdc17daef
Durgaprasad-MDP <durgaprasad.midathani@gmail.com>
2024-06-24 16:07:20 +0530

    commit1

---
+++ src/pd1/Opportunity.java
@@ -0,0 +1,17 @@
+package pd1;
+
+public class Opportunity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hello World");
+	}
+
+}
+
+
+
+
+
